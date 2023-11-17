d3f279e7450895d8bd80393076e57516055bf4e5
eugabrielm <eumoreiragabriel@gmail.com>
2023-11-17 08:58:06 -0300

    Pacman - Base do projeto

---
+++ cmake-build-debug/CMakeFiles/p5-pacman.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Actor.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Actor.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Actor.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Ghost/Ghost.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/Ghost.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/Ghost.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Ghost/States/ChaseState.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/ChaseState.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/ChaseState.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Ghost/States/DeadState.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/DeadState.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/DeadState.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Ghost/States/FrightenedState.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/FrightenedState.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/FrightenedState.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Ghost/States/GhostState.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/GhostState.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/GhostState.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Ghost/States/ScatterState.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/ScatterState.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Ghost/States/ScatterState.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Item.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Item.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Item.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Pacman.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Pacman.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Pacman.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/PathNode.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/PathNode.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/PathNode.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Actors/Wall.cpp" "CMakeFiles/p5-pacman.dir/Source/Actors/Wall.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Actors/Wall.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/AIComponents/FSMComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/AIComponents/FSMComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/AIComponents/FSMComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/AIComponents/FSMState.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/AIComponents/FSMState.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/AIComponents/FSMState.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/ColliderComponents/AABBColliderComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/ColliderComponents/AABBColliderComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/ColliderComponents/AABBColliderComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/ColliderComponents/CircleColliderComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/ColliderComponents/CircleColliderComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/ColliderComponents/CircleColliderComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/Component.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/Component.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/Component.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/DrawComponents/DrawAnimatedComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawAnimatedComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawAnimatedComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/DrawComponents/DrawComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/DrawComponents/DrawPolygonComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawPolygonComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawPolygonComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/DrawComponents/DrawSpriteComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawSpriteComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/DrawComponents/DrawSpriteComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Components/RigidBodyComponent.cpp" "CMakeFiles/p5-pacman.dir/Source/Components/RigidBodyComponent.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Components/RigidBodyComponent.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Game.cpp" "CMakeFiles/p5-pacman.dir/Source/Game.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Game.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Main.cpp" "CMakeFiles/p5-pacman.dir/Source/Main.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Main.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Math.cpp" "CMakeFiles/p5-pacman.dir/Source/Math.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Math.cpp.obj.d"
+  "D:/Downloads/p5-pacman-eugabrielm-main/Source/Random.cpp" "CMakeFiles/p5-pacman.dir/Source/Random.cpp.obj" "gcc" "CMakeFiles/p5-pacman.dir/Source/Random.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
